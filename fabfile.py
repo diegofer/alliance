21e864c98b5cab1c7a748620420a50e965532ec2
Dieguisimo <diegofernando83@gmail.com>
2013-06-17 22:31:36 -0500

    Preparando para primer deploy en servidor de stagin con fabric

--- fabfile.py
+++ fabfile.py
@@ -1,38 +1,65 @@
 from fabric.api import * 
+from fabric.contrib.console import confirm
 from fabric.colors import green, red 
 
 import os
 import re
 
 RUTA_PROYECTO = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+#env.hosts = ['localhost', 'alianza@web348.webfaction.com']
 
 
+def servidor_local():
+	env.hosts = ['localhost']
 
 
 def init():
 	
 	initSentry()
 	initAlliance()
-	#versionar_html()
-	# initAlliance()
-
-	#local(django_manage("./manage.py runserver", "alliance"))
-	# with prefix('WORKON_HOME=$HOME/.virtualenvs'):
-	# 	with prefix('source /usr/local/bin/virtualenvwrapper.sh'):
-	# 		with prefix('workon myenv'): # Assuming there is a env called `myenv`
-	# 			run('pip freeze')
-
-	#local('sudo ~/virtualenvs/alliance/bin/activate')
-	# with lcd('/host/dev/django/alliance'), prefix('workon alliance'):
-	# 	local('./manage.py runserver')
-		#local('./manage.py loaddata myfixture')
-	# env.hosts = ['localhost']
-	# run("workon")
-	#with cd('/host/dev/django/alliance'):
-	#local('/usr/local/bin/virtualenvwrapper.sh && workon alliance')
-	#local("./manage.py runserver")
 
 
+def alistar_actualiacion():
+	versionar_home()
+
+
+
+def actualizar_stage():
+	with cd('/var/www/alliance'):
+		run('git pull')
+		run('./manage.py collectstatic')
+
+	# Reiniciar apache
+	run('sudo /etc/init.d/apache2 restart')
+
+
+# Git 
+def commit():
+	local("git add -p && git commit")
+
+
+
+def test():
+	local(django_manage('./manage.py test'))
+
+
+
+
+
+def initSentry():
+	with lcd("/host/dev/django/sentry"):
+		local(django_manage("sentry --config=conf.py start", "sentry")+" &")
+		#local('xdg-open http://0.0.0.0:9000/')
+
+def initAlliance():
+	with lcd("/host/dev/django/alliance"):
+		local(django_manage("./manage.py runserver & xdg-open http://127.0.0.1:8000/"))
+	
+def django_manage(command='help', virtualenv='alliance'):
+	return "/bin/bash -l -c 'source /usr/local/bin/virtualenvwrapper.sh && workon " + virtualenv + " && " + command + "'"
+
+
+# VERSIONAR
 def versionar_home():
 	plantilla = os.path.join(RUTA_PROYECTO, 'main/templates/main/home.html')
 	print plantilla
@@ -51,21 +78,3 @@ def versionar_home():
 	archivo_actualizado = open(plantilla, 'w')
 	archivo_actualizado.writelines(lineas_actualizadas)
 	archivo_actualizado.close()
-
-
-
-
-def django_manage(command='help', virtualenv='alliance'):
-	return "/bin/bash -l -c 'source /usr/local/bin/virtualenvwrapper.sh && workon " + virtualenv + " && " + command + "'"
-
-
-
-def initSentry():
-	with lcd("/host/dev/django/sentry"):
-		local(django_manage("sentry --config=conf.py start", "sentry")+" &")
-		#local('xdg-open http://0.0.0.0:9000/')
-
-def initAlliance():
-	with lcd("/host/dev/django/alliance"):
-		local(django_manage("./manage.py runserver"))
-		#local('xdg-open http://127.0.0.1:8000/')
