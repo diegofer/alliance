e73a5be69e7b6b719d120f4eb381ea1241849157
Dieguisimo <diegofernando83@gmail.com>
2013-10-04 11:47:54 -0500

    El grafico de regiones ya resalta cada slice cuando se hace over en region en el mapa

--- main/static/main/app/js/views/RegionListChartView.js
+++ main/static/main/app/js/views/RegionListChartView.js
@@ -9,6 +9,13 @@ function($, _, Backbone, tastypie) {
    
     var ChartView = Backbone.View.extend({
 
+        dt          : new google.visualization.DataTable(),
+        opt         : {},
+        colors      : [],
+        indexRegion : {},
+
+
+
         initialize: function() {  
         	console.log('Inicializando vista ChartView'); 
             //this.listenTo(app.regionListView, 'clickRegion', this.selectRegion);
@@ -21,26 +28,20 @@ function($, _, Backbone, tastypie) {
         },
 
         dibujarChart: function() {        
-            var dt = new google.visualization.DataTable();
-            dt.addColumn('string', 'Region');
-            dt.addColumn('number', 'Iglesias');
-
-            var indexRegion = {};
-            var colors = [];
+            this.dt.addColumn('string', 'Region');
+            this.dt.addColumn('number', 'Iglesias');
 
             this.collection.each(function(data) {
-                var index = dt.addRow([data.get('nombre'), data.get('iglesias')]);
-                indexRegion[data.get('nombre')] = index;
-                colors.push(data.get('color'));
-            });
+                var index = this.dt.addRow([data.get('nombre'), data.get('iglesias')]);
+                this.indexRegion[data.get('nombre')] = index;
+                this.colors.push(data.get('color'));
+            }, this);
 
-            this.indexRegion = indexRegion;
-
-            var opt = { 
+            this.opt = { 
                 title: 'Iglesias por cada Región', 
                 pieHole: 0.4,
                 backgroundColor: 'white',
-                colors: colors,
+                colors: this.colors,
                 chartArea: {
                     left:20,
                     top: 40,
@@ -51,20 +52,28 @@ function($, _, Backbone, tastypie) {
                 tooltip: {
                     showColorCode: true,
                     trigger: 'focus',
-                }
+                },
+                pieStartAngle: 180,
+                slices: {},
             };
 
             this.chart = new google.visualization.PieChart(this.el);
-            this.chart.draw(dt, opt);
+            this.chart.draw(this.dt, this.opt);
         },
 
-        selectRegion: function(nombre) {  
+        selectRegion: function(nombre) {             
             var rowIndex = this.indexRegion[nombre];
+            this.opt.slices[rowIndex] = {offset: 0.1};
+            this.chart.clearChart();
+            this.chart.draw(this.dt, this.opt);
+
             this.chart.setSelection([{row:rowIndex}]);
         },
 
         deselectRegion: function() {
-            this.chart.setSelection(null);
+            this.opt.slices = {};
+            this.chart.clearChart();  // este metodo elimina todo, la seleccion tambien
+            this.chart.draw(this.dt, this.opt);
         },
         
 
